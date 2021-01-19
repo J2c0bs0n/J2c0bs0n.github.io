0cff1b9fda8e68d701a0c60ae6dbf28ae3a313ee
J2c0bs0n <77638059+J2c0bs0n@users.noreply.github.com>
2021-01-19 13:35:31 -0600

    idk

--- Template/sketch.js
+++ Template/sketch.js
@@ -1,9 +1,8 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Bouncing Balls
+//Array Demo
+
+let theBouncy = [];
+
 
 
 function setup() {
@@ -11,6 +10,41 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  //
+  background("grey");
+  moveBall();
+  displayBall();
+}
+
+function mousePressed(){
+  let ball = {
+  x: mouseX,
+  y: mouseY,
+  diameter: random(25,100),
+  dx: random(-5, 5),
+  dy: random(-5, 5),
+  theColor: color(random(255),random(255),random(255),random(255)),
+};
+  theBouncy.push(ball);
+}
+function moveBall(){
+  for (let ball of theBouncy){
+     ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  if (ball.x + ball.diameter / 2 >= width || ball.x - ball.diameter / 2<= 0){
+    ball.dx *= -1;
+  }
+  if (ball.y + ball.diameter / 2 >= height || ball.y - ball.diameter / 2<= 0){
+    ball.dy *= -1;
+    }
+  }
+ 
+}
+function displayBall(){
+  for (let ball of theBouncy){
+    noStroke();
+    fill(theBouncy.theColor);
+    ellipse(ball.x,ball.y,ball.diameter,ball.diameter);
+  }
+  
 }
