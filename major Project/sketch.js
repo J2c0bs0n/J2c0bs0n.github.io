68da5df1272ace09919bfc5994dbf7c11cc4f8a9
J2c0bs0n <77638059+J2c0bs0n@users.noreply.github.com>
2021-02-08 14:10:20 -0600

    added introduction

--- major Project/sketch.js
+++ major Project/sketch.js
@@ -1,8 +1,40 @@
-//Template
+//Major Project
+
+let name;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background("black");
+  displayText();
+  keyPressed();
+  beginName();
+}
+
+function displayText(){
+  textSize(20);
+  stroke("black");
+  fill("white");
+  text("Welcome to Norcay! Put in begin to start the game!", 559, 384);
+}
+
+function keyPressed(){
+  if (key === " "){
+    createCanvas(windowWidth, windowHeight);
+    background("black");
+    textSize(20);
+    name= prompt("What is your name?", 659, 384);
+    return name;
+  }
+}
+
+function beginName(){
+  if (name[0] === "a" || name[0] === 1){
+    textSize(20);
+    stroke("black");
+    fill("white");
+    text("Let your journey begin!", 559, 384);
+  }
 }
