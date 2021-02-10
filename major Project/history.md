50533c8353fdd9e3e32561b61e570c8b08f5ba51
J2c0bs0n <77638059+J2c0bs0n@users.noreply.github.com>
2021-02-10 13:32:50 -0600

    filler text

--- major Project/history.md
+++ major Project/history.md
@@ -2,3 +2,6 @@
 
 ## 2/9/2021
   - Added a means of inputting your response
+
+## 2/10/2021
+  - Encountered problem where program isn't running properly. Currently fixing
--- major Project/sketch.js
+++ major Project/sketch.js
@@ -1,9 +1,7 @@
 //Major Project
-let choice;
-
 let name;
 
-let theGameBegin = 0;
+let choice;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,39 +9,39 @@ function setup() {
 
 function draw() {
   background("black");
-  displayText();
-  keyPressed();
   chapter1();
 }
 
-function displayText(){
-  textSize(20);
-  stroke("black");
-  fill("white");
-  text("Welcome to Norcay! Press the spacebar to start the game!", 559, 384);
-}
-
 function keyPressed(){
+  while (key !== "a" && key !== " "){
+    textSize(20);
+    stroke("black");
+    fill("white");
+    text("Welcome to Norcay! Press the spacebar to start the game!", 559, 384);
+  }
   if (key === " "){
     createCanvas(windowWidth, windowHeight);
     background("black");
     textSize(20);
     name = prompt("What is your name?");
-    return name;
   }
-  if (key === "a" && theGameBegin === 1){
-    textSize(20);
-    stroke("black");
-    fill("white");
-    text("You wake up in a thick forest. You know not of who you are, where you are, or why you're there.", mouseX, mouseY);
-    choice = prompt("What do you do?");
+  if (name !== undefined){
+    if (key === "a"){
+      textSize(15);
+      stroke("black");
+      fill("white");
+      choice = prompt("What do you do?");
+    }
+    
   }
 }
 
 function chapter1(){
-  textSize(20);
-  stroke("black");
-  fill("white");
-  text("Hello " +name+ "!To begin the game, press the a button.", mouseX, mouseY);
-  theGameBegin === 1;
+  if (key !== "a"){
+      textSize(20);
+      stroke("black");
+      fill("white");
+      text("Hello " +name+ "! To begin the game, press the a button.", mouseX, mouseY);
+
+  }
 }
