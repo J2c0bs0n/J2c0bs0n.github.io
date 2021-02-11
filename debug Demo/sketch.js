678b61623a98c6faeb4dd1fb2eabc42f611b918f
J2c0bs0n <77638059+J2c0bs0n@users.noreply.github.com>
2021-02-11 14:08:24 -0600

    Updated game and proposal

---
+++ debug Demo/sketch.js
@@ -0,0 +1,13 @@
+//Template
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  x=width/2;
+  y=height/2;
+}
+
+function draw() {
+  background(220);
+
+  rect(x,y, mouseX, mouseY);
+}
--- major Project/sketch.js
+++ major Project/sketch.js
@@ -1,9 +1,15 @@
-//Major Project
-let dialogueText = ["What do you do?","What is your name?", "You wake up in a dense forest, of which you don't recognize. You don't know who you are, or where you came from. All you know, is that you need to get out."];
+//Norcay
+//Austin Connell
+//2/11/2021
+let dialogueText = ["What do you do?","What is your name?", "You wake up in a dense forest, of which you don't recognize. You don't know who you are, or where you came from. All you know, is that you need to get out.", "You see a wall of trees.","You see an empty tree stump, but nothing else of notice.","You see a path ahead of you.","You appear to be in an open patch of grass. You don't really see much else.","You see a key inside. It looks normal. Do you want pick it up?","You now have a key. You go back to where you were before.","Got it, you go back to where you were standing before.","The stump is empty, because you took the key.","You see the key you picked up. It looks like an average key, but something seems off for some reason",];
 
 let name;
 
 let choice;
+
+let counter = 0;
+
+let theKey= 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -28,23 +34,52 @@ function keyPressed() {
     fill("white");
     choice = prompt(dialogueText[2]);
     if (choice !== undefined){
-      choice = prompt(dialogueText[0])
+      choice = prompt(dialogueText[0]);
     }
-    else if(choice === "" || choice === " "){
-      choice = prompt("Please put in an awnser " +name+ ".");
-    }
+    
   }
 }
 
 function chapter1() {
-  if (key !== "a" && key === " ") {
+  if (key !== "a" && key === " "){
     textSize(20);
     stroke("black");
     fill("white");
-    text("Hello " + name + "! To begin the game, press the a button.", mouseX, mouseY);
+    text("Hello " + name + "! To begin the game, press the a button. To remove this message, cancel the prompt, then press a.", mouseX, mouseY);
   }
-  if (choice === "turn left"){
-    choice = prompt ("You see a wall of trees, what do you do?");
+  if (choice === "look west"){
+    choice = prompt (dialogueText[3]);
+  }
+  else if (choice === "look south"){
+    choice = prompt(dialogueText[4])
+  }
+  else if (choice === "look north"){
+    choice = prompt(dialogueText[5]);
+  }
+  else if (choice === "look east"){
+    choice = prompt(dialogueText[3]);
+  }
+  else if (choice === "look around"){
+    choice = prompt (dialogueText[6])
+  }
+  else if (choice === "go to tree stump"){
+    choice = prompt(dialogueText[7])
+    counter += 1;
+  }
+  else if (choice === "yes" && counter === 1){
+    choice = prompt(dialogueText[8])
+    counter -=1;
+    theKey += 1;
+  }
+  else if(choice === "no" && counter === 1){
+    choice = prompt(dialogueText[9]);
+    counter -= 1;
+  }
+  else if (choice === "go to the tree stump" && theKey === 1){
+    choice = prompt(dialogueText[10]);
+  }
+  else if (choice === "check key"){
+    choice = prompt(dialogueText[11])
   }
 }
 
